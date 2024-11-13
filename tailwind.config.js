a9b205f3a246608370cf01802df9574500c1efc9
AKM04 <abdulghanim70@gmail.com>
2024-11-13 16:13:24 +0300

    style: :lipstick: screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       container: {
         center: true,
-        padding: '1rem',
+        padding: "1rem",
       },
       colors: {
-        'primary': '#EC8A20',    // Orange
-        'secondary': '#C98142',  // Brown/Orange
-        'accent': '#38C5CA',     // Turquoise
-        'light': '#FEFAE1', 
-        'light-cream': '#F0D4A9',
+        primary: "#EC8A20", // Orange
+        secondary: "#C98142", // Brown/Orange
+        accent: "#38C5CA", // Turquoise
+        light: "#FEFAE1",
+        "light-cream": "#F0D4A9",
       },
       fontFamily: {
-        'sans': ['Roboto', 'sans-serif'],
-        'roboto': ['Roboto', 'sans-serif'],
-        'item': ['IBM Plex Sans Arabic', 'sans-serif'],
+        sans: ["Roboto", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        item: ["IBM Plex Sans Arabic", "sans-serif"],
+      },
+      screens: {
+        xs: "475px",
+        sm: { max: "639px" },
       },
     },
   },
   plugins: [],
-}
+};
